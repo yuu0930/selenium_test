19577a21cab7449de6344c4477d129a421055568
yuu0930 <rixxn.haruka.loooove@gmail.com>
2025-08-27 00:21:13 +0900

    FizzBuzz問題の実装

---
+++ fizzbuzz.py
@@ -0,0 +1,12 @@
+def fizzbuzz():
+    for n in range(1, 101):
+        if n % 15 == 0:
+            print("FizzBuzz")
+        elif n % 5 == 0:
+            print("Buzz")
+        elif n % 3 == 0:
+            print("Fizz")
+        else:
+            print(n)
+        
+fizzbuzz()
