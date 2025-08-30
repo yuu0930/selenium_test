9397fc12ceae12fff86f1ca61495e46509443465
yuu0930 <rixxn.haruka.loooove@gmail.com>
2025-08-31 07:22:15 +0900

    [ADD]sortedメソッド、reverseメソッド、popメソッドの追加

--- list_test.py
+++ list_test.py
@@ -12,3 +12,22 @@ print(fourth_to_seventh)
 
 number_list = number.split("-")     # "対象の文字列".split("区切り文字")＝指定した文字で区切ったリストを取得できる
 print(number_list)
+
+join_number = "-".join(number_list) # リストの各要素をある区切り文字で結合して文字列を作るには文字列のjoinメソッドを使用する
+print(join_number)
+
+numbers = [3, 1, 4, 2, 5]
+sorted_numbers = sorted(numbers)    # sorted関数は昇順に並び替える
+print(sorted_numbers[:3])
+
+numbers.reverse()                   # reverseはリストの要素を逆順にする
+print(numbers)
+
+sum_numbers = sum(numbers)          # リストの合計値
+max_numbers = max(numbers)          # リストの最大値
+min_numbers = min(numbers)          # リストの最小値
+print(sum_numbers, max_numbers, min_numbers)
+
+second_number = numbers.pop(1)      # popメソッドは指定した値を削除する,この場合2を削除し、second_numberには削除した2が代入される
+print(numbers)
+print(second_number)
